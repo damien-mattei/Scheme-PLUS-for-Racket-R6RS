4e181e4a6b57cedeca9a64c78f9b27f7a9fd49bd
Damien MATTEI <damien.mattei@gmail.com>
2024-08-20 11:40:16 +0200

    documentation , PLT package

--- info.rkt
+++ info.rkt
@@ -2,10 +2,13 @@
 
 (define pkg-name "Scheme-PLUS-for-Racket-R6RS") ; "+" not allowed in package names
 (define collection "Scheme+")
-(define compile-omit-paths '("deprecated"))
+(define compile-omit-paths '("deprecated" "compiled"))
+(define test-omit-paths '("compiled"))
 (define pkg-desc "Scheme+ for Racket R6RS")
 (define version "9.1")
 (define pkg-authors '(mattei))
+(define scribblings '(("scribblings/Scheme-PLUS-for-Racket-R6RS.scrbl" ())))
+(define build-deps '("scribble-lib" "racket-doc" "scribble-code-examples" "scribble-doc"))
 (define license 'GPL-3.0-or-later)
 
 (define deps
@@ -13,8 +16,4 @@
     "r6rs-lib"
     "srfi-lib"
     "sci"))
-;;"reprovide-lang-lib"))
-
-(define build-deps
-  '())
 
---
+++ scribblings/Scheme-PLUS-for-Racket-R6RS.scrbl
@@ -0,0 +1,12 @@
+#lang scribble/manual
+
+@title{Scheme+ for Racket R6RS}
+
+Install as a package with the package manager.
+ 
+Use in an R6RS module:
+
+(import Scheme+)
+
+
+@hyperlink["See general documentation of Scheme+"]{https://github.com/damien-mattei/Scheme-PLUS-for-Racket/blob/gh-pages/README.md}
